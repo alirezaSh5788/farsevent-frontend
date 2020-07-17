031bc29c9206c88f0652af72610334a9f234e2f3
alireza sh <ali.sheikh5788@gmail.com>
2020-07-17 15:11:10 +0430

    show some category in homepage

---
+++ src/components/home/ContentComponents/PopularCategory/PopularCategory.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="category-section mb-5">
+    <div class="row">
+      <div class="col-12 col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 p-3 cate">
+        <div class="category">
+          <h3>کار افرینی</h3>
+        </div>
+      </div>
+      <div class="col-12 col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 p-3 cate">
+        <div class="category">
+          <h3>تکنولوژِی</h3>
+        </div>
+      </div>
+      <div class="col-12 col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 p-3 cate">
+        <div class="category">
+          <h3>هنری</h3>
+        </div>
+      </div>
+      <div class="col-12 col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 p-3 cate">
+        <div class="category">
+          <h3>کسب و کار</h3>
+        </div>
+      </div>
+      <div class="col-12 col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 p-3 cate">
+        <div class="category">
+          <h3>مدیریت</h3>
+        </div>
+      </div>
+      <div class="col-12 col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 p-3 cate">
+        <div class="category">
+          <h3>مهندسی</h3>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+</style>
---
+++ src/components/home/ContentComponents/PopularCategory/PopularCategoryTitle.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="trending-category d-flex">
+    <h3 class="ml-3">محبوب ترین های دسته بندی ها</h3>
+    <a href="#" class="mt-2">مشاهده ی همه</a>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+</style>
