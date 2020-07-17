46a2e4f2fcc37ebaec5d98331d46641474377ded
alireza sh <ali.sheikh5788@gmail.com>
2020-07-17 14:38:09 +0430

    Create a footer for all web pages

---
+++ src/components/home/Footer.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="footer-section pt-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-3 col-xl-3">
+          <h4>موضوع رویداد ها</h4>
+          <br />
+          <ul>
+            <a href="#">
+              <li>رویداد های تکنولوژی</li>
+            </a>
+            <a href="#">
+              <li>رویداد های فرهنگی</li>
+            </a>
+            <a href="#">
+              <li>رویداد های سیاسی</li>
+            </a>
+            <a href="#">
+              <li>رویداد های مذهبی</li>
+            </a>
+            <a href="#">
+              <li>رویداد های قومی</li>
+            </a>
+          </ul>
+        </div>
+        <div class="col-lg-3 col-xl-3">
+          <h4>انواع رویداد ها</h4>
+          <br />
+          <ul>
+            <a href="#">
+              <li>وبینار</li>
+            </a>
+            <a href="#">
+              <li>حضوری</li>
+            </a>
+            <a href="#">
+              <li>غیره</li>
+            </a>
+          </ul>
+        </div>
+        <div class="col-lg-3 col-xl-3">
+          <h4>شهر ها</h4>
+          <br />
+          <ul>
+            <a href="#">
+              <li>تهران</li>
+            </a>
+            <a href="#">
+              <li>مشهد</li>
+            </a>
+            <a href="#">
+              <li>اصفهان</li>
+            </a>
+            <a href="#">
+              <li>شیراز</li>
+            </a>
+            <a href="#">
+              <li>تبریز</li>
+            </a>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <br />
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+</style>
