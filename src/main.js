7452fa5c0d80b543a917d3b71165d9ded9b96e77
alireza sh <ali.sheikh5788@gmail.com>
2020-07-17 15:41:50 +0430

    som style to project

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import store from "./store";
+import router from './router';
+import Vuelidate from 'vuelidate';
+import { Datetime } from 'vue-datetime'
+import 'vue-datetime/dist/vue-datetime.css'
+import { VueperSlides, VueperSlide } from "vueperslides";
+import "vueperslides/dist/vueperslides.css";
 
+Vue.use(Datetime)
+Vue.use(Vuelidate);
+Vue.component('datetime', Datetime);
+Vue.component("vueper", VueperSlides);
 new Vue({
-  el: '#app',
+  el: "#app",
+  store,
+  router,
   render: h => h(App)
-})
+});
