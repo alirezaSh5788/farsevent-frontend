25057dbfb0136d018e4d8cd25998ccb0ebb0fe4d
alireza sh <ali.sheikh5788@gmail.com>
2020-07-17 14:43:35 +0430

    Create a Facilities for homepage of website

---
+++ src/components/home/Facilities.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="b2b-section">
+    <div class="container">
+      <h4 class="my-5">ابزارهایی که ایوند در اختیار برگزارکنندگان قرار می‌دهد</h4>
+      <div class="row box-discription p-5">
+        <div class="col-12 col-xs-12 col-sm-12 col-md-8 col-lg-8 col-xl-8">
+          <h4>ایجاد رویداد</h4>
+          <p>
+            می‌توانید به سادگی رویدادتان را در ایوند ایجاد کرده و -ضمن دریافت یک صفحه با آدرس اختصاصی-
+            اطلاعات مختلفی مثل زمان برگزاری، مکان رویداد همراه با نقشه‌ی گوگل، قیمت بلیت‌ها و سایر
+            مشخصات
+            رویداد را در معرض دید مخاطبین قرار دهید.
+          </p>
+        </div>
+        <div class="col-12 col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+          <img src="img/b2b.svg" class="w-100" alt />
+        </div>
+        <button class="btn mx-auto my-3">ثبت نام</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+</style>
