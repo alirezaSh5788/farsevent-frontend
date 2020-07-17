1e97ff5c52fcc1cd9e03243ac80958027e0f94c8
alireza sh <ali.sheikh5788@gmail.com>
2020-07-17 14:58:04 +0430

    add some feature to home page

---
+++ src/components/home/AppContentEvents.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <app-corona-box></app-corona-box>
+    <app-title-popular-events></app-title-popular-events>
+    <app-popular-events-table></app-popular-events-table>
+    <app-popular-category-title></app-popular-category-title>
+    <app-popular-category></app-popular-category>
+  </div>
+</template>
+<script>
+import CoronaBox from "./contentComponents/CoronaBox";
+import TitlePopularEvents from "./contentComponents/TitlePopularEvents.vue";
+import PopularEventsTable from "./contentComponents/PopularEventsTable";
+import PopularCategoryTitle from "./contentComponents/PopularCategory/PopularCategoryTitle";
+import PopularCategory from "./contentComponents/PopularCategory/PopularCategory";
+export default {
+  components: {
+    appCoronaBox: CoronaBox,
+    appTitlePopularEvents: TitlePopularEvents,
+    appPopularEventsTable: PopularEventsTable,
+    appPopularCategoryTitle: PopularCategoryTitle,
+    appPopularCategory: PopularCategory
+  }
+};
+</script>
+<style scoped>
+</style>
---
+++ src/components/home/ContentComponents/CoronaBox.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="about-box mb-5">
+      <div class="row">
+        <div class="col-12 col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-3 mx-auto">
+          <img src="img/doctor.svg" alt />
+        </div>
+        <div class="col-12 col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-3">
+          <h4>تیم در زمان کورنا</h4>
+          <p>
+            تیم ایوند از هفته دوم اسفند به صورت دورکاری داره به کار خودش ادامه میده و برای بهبود سیستم
+            رویداد تلاش میکنه.
+          </p>
+          <button class="btn">ثبت نام</button>
+        </div>
+        <div class="col-12 col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-3">
+          <h4>تیم در زمان کورنا</h4>
+          <p>
+            تیم ایوند از هفته دوم اسفند به صورت دورکاری داره به کار خودش ادامه میده و برای بهبود سیستم
+            رویداد تلاش میکنه.
+          </p>
+          <button class="btn">ثبت نام</button>
+        </div>
+        <div class="col-12 col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-3">
+          <h4>تیم در زمان کورنا</h4>
+          <p>
+            تیم ایوند از هفته دوم اسفند به صورت دورکاری داره به کار خودش ادامه میده و برای بهبود سیستم
+            رویداد تلاش میکنه.
+          </p>
+          <button class="btn">ثبت نام</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+</style>
